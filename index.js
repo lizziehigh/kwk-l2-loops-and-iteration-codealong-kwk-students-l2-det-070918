546a72ae553d8246f10e79c54a37267b3336a281
lizziehigh <lizziehigh02@gmail.com>
2018-07-12 15:39:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+function printBadges(array) {
+  for(var i = 0; i < array.length; i++) {
+    console.log("Welcome " + array[i] + "! You are employee #" + (i + 1) + ".")
+  
+}
+return array
+}
+
+function tailsNeverFails() {
+  heads = false
+  tails = 0
+  while (heads === false) {
+    var flip = Math.random()
+  if (flip >= .5) {
+    tails++
+  }
+  else {
+    heads = true
+  }
+    
+  }
+  return "You got " + tails + " tails in a row!"
+}
