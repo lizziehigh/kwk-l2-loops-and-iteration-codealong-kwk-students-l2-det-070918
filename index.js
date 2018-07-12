84fa1cf6d83490c8f070e75690474e5301cf6abb
lizziehigh <lizziehigh02@gmail.com>
2018-07-12 15:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function printBadges(array) {
+  for(var i = 0; i < array.length; i++) {
+    console.log("Welcome, " + array[i] + "!" + "you are employee #" + i)
+  
+}
+return array
+}
+
+function tailsNeverFails() {
+  heads = false
+  tails = 0
+  while (heads === 0) {
+    var flip = Math.random()
+  if (flip == 1) {
+    heads++
+  }
+    
+  }
+}
